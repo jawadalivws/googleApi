88c5c1bd4d42d9e05c146ad1c91a463ff4a928a6
jawadalivws <145324513+jawadalivws@users.noreply.github.com>
2023-12-08 10:55:55 +0500

    ss

--- app/Console/Commands/googleSearchApi.php
+++ app/Console/Commands/googleSearchApi.php
@@ -70,8 +70,8 @@ class googleSearchApi extends Command
     
                         // }else{
                             $response = HTTP::get('https://www.googleapis.com/customsearch/v1' , [
-                                'key' => "AIzaSyB0BGGeRNKp9Y_Sb8kLy4DCxOELJ3tQdro",
-                                'cx' => "41f26495cdd8c4ed4",
+                                'key' => "AIzaSyAqR9Ns-HoUIyOnootGky7NgFaWLilsnmU",
+                                'cx' => "432d043d77144425f",
                                 'q' => $word->name,
                                 'start' => ($page - 1) * $results_per_page + 1, // Calculate the starting index for the current page
                                 'num' => $results_per_page,
