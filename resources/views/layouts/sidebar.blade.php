c8e4a3b3cab0b04a57a626b9ed5ddec636895f53
jawadalivws <Jawadali@vizzweb.com>
2024-03-05 17:57:48 +0500

    a

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <a href="/"><img src="{{ asset('assets/images/icon/logo.png')}}" alt="logo"></a>
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+                            <li class="{{ request()->is('/') ? 'active' : '' }}">
+                                <a href="/"><i class="ti-dashboard"></i><span>Keyword List</span></a>
+                            </li>
+                            <li class="{{ request()->is('email/list') ? 'active' : '' }}">
+                                <a href="/email/list"><i class="ti-email"></i><span>Email List</span></a>
+                            </li>
+                            <li class="{{ request()->is('setting') ? 'active' : '' }}">
+                                <a href="/setting"><i class="ti-id-badge"></i><span>Setting</span></a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
