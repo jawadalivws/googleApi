e25f637d59462f280f3009eac430e417d0b121fc
jawadalivws <Jawadali@vizzweb.com>
2024-04-24 13:54:55 +0500

    changes

--- resources/views/keyword_detail.blade.php
+++ resources/views/keyword_detail.blade.php
@@ -59,6 +59,11 @@
         </div>
                 
     </form> -->
+    <div class="row mb-4">
+        <div class="col-md-4">
+            <h3>Keyword Detail</h3>
+        </div>
+    </div>
     <div class="row">
         <div class="col-12">
             <div class="card">
