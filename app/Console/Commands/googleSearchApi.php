8bb29c6aa867b7eefcd14b56a43a83ee5a6646f3
jawadalivws <Jawadali@vizzweb.com>
2024-05-08 23:53:06 +0500

    dd

--- app/Console/Commands/googleSearchApi.php
+++ app/Console/Commands/googleSearchApi.php
@@ -204,30 +204,30 @@ class googleSearchApi extends Command
                 if (!empty($dataToInsert)) {
                     \Log::info("data insert");
                     KeywordRecord::insert($dataToInsert);
-                    // foreach($dataToInsert as $data){
-                    //     // dump($data['email']);
-                    //     $curl = curl_init();
+                    foreach($dataToInsert as $data){
+                        // dump($data['email']);
+                        $curl = curl_init();
 
-                    //     curl_setopt_array($curl, array(
-                    //     CURLOPT_URL => 'https://email.updatemedaily.com/campaigns/add_campaign_email',
-                    //     CURLOPT_RETURNTRANSFER => true,
-                    //     CURLOPT_ENCODING => '',
-                    //     CURLOPT_MAXREDIRS => 10,
-                    //     CURLOPT_TIMEOUT => 0,
-                    //     CURLOPT_FOLLOWLOCATION => true,
-                    //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    //     CURLOPT_CUSTOMREQUEST => 'POST',
-                    //     CURLOPT_POSTFIELDS => array('campaign_id' => $campaign_id,'contact_email' => $data['email']),
-                    //     CURLOPT_HTTPHEADER => array(
-                    //         'Cookie: ci_session=p24kmm1qgsn7dnnlilifndr6a7s3g7kc'
-                    //     ),
-                    //     ));
+                        curl_setopt_array($curl, array(
+                        CURLOPT_URL => 'https://email.updatemedaily.com/campaigns/add_campaign_email',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => '',
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 0,
+                        CURLOPT_FOLLOWLOCATION => true,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => 'POST',
+                        CURLOPT_POSTFIELDS => array('campaign_id' => $campaign_id,'contact_email' => $data['email']),
+                        CURLOPT_HTTPHEADER => array(
+                            'Cookie: ci_session=p24kmm1qgsn7dnnlilifndr6a7s3g7kc'
+                        ),
+                        ));
                 
-                    //     $response = curl_exec($curl);
+                        $response = curl_exec($curl);
                 
-                    //     curl_close($curl);
-                    //     echo $response;
-                    // }
+                        curl_close($curl);
+                        echo $response;
+                    }
                 }
             }  
         }
