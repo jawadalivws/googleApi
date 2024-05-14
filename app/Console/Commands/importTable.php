2ebeb00ba996eca2903f0b0e6d99d57c5b393972
jawadalivws <Jawadali@vizzweb.com>
2024-05-14 21:09:34 +0500

    sql table command

---
+++ app/Console/Commands/importTable.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class importTable extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'importTable';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import a SQL table into the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $tableName = $this->argument('countires');
+
+        $sqlDumpPath = storage_path('tables/'.$tableName.'.sql');
+
+        $sql = file_get_contents($sqlDumpPath);
+
+        DB::unprepared($sql);
+
+        $this->info("Table $tableName imported successfully!");
+        
+        return 0;
+    }
+}
